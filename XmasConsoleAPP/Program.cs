e83bd1eff9e3eb2a7a98954d2b7eb54ff03a2356
Tarek <tarekakkawi89@gmail.com>
2017-12-21 16:59:45 +0000

    xmas app

---
+++ XmasConsoleAPP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace XmasConsoleAPP
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            //constructor
+            XmasApp xa = new XmasApp();
+
+            Console.ReadLine();
+
+        }
+
+        
+
+       
+    }
+}
+
+    
+       
+        
+
+
+
+
+        
+    
+
---
+++ XmasConsoleAPP/XmasApp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace XmasConsoleAPP
+{
+    public class XmasApp
+    {
+        Timer t;
+        
+        public XmasApp()
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            t = new Timer();
+            t.Elapsed += XmasTree;
+            t.Interval = 1000;
+            t.Enabled = true;
+            t.Start();
+
+        }
+
+        private  void XmasTree(object sender, ElapsedEventArgs e)
+        {
+
+
+            if (DateTime.Now.Minute == 50)
+            {
+                t.Stop();
+                int x = 20, y, z;
+
+                Console.WriteLine("::::::::::::::: Merry Xmas :::::::::::::::");
+
+
+                for (int i = 1; i <= x; i++) // Total number of layer for pramid
+
+                {
+
+                    for (y = 1; y <= (x - i); y++)  // Loop For Space
+                        
+                        Console.Write(" ");
+                    
+
+                    for (z = 1; z <= i; z++) //increase the value
+
+                        if (z % 2 == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("@");
+                           
+                        }
+                        
+                        
+                        else 
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("*");
+                        }
+
+                    for (z = (i - 1); z >= 1; z--)  //decrease the value
+
+                        if (z % 2 == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("@");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("*");
+                        }
+
+                    
+                    Console.WriteLine();
+
+                }
+                
+            }
+            else
+            {
+               
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(50, 15);// makes it one line (timer) instead of one per line
+                Console.WriteLine(DateTime.Now);
+            }
+        }
+
+    }
+}
